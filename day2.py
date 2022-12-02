90bffdc069d4ee5c21bb1d3f643b38fe0f36baef
Kittenlover229 <artur.roos@mail.ru>
2022-12-02 20:52:34 +0200

    Day 2, part 2

--- day2.py
+++ day2.py
@@ -12,17 +12,23 @@ for line in open("inputs.txt").readlines():
         # 3 + 1 - 2 = 2 = 2 paper vs scissors
         # 3 + 2 - 2 = 3 = 0 scissors vs scissors
 
-        # 0 is a draw
-        # 1 is a win
-        # 2 is a loss
+        # +1 to roll the array
+        # 0 is a loss
+        # 1 is a draw
+        # 2 is a win
 
         i = (3 + me - them + 1) % 3
-        print(x:=[0, 3, 6][i], y:=[1, 2, 3][me], z:=x + y)
-        return z 
+        print(x := [0, 3, 6][i], y := me + 1, z := x + y)
+        return z
+
+    def solve_2(me, them) -> int:
+        i = (them + me - 1) % 3
+        print(x := i + 1, y := [0, 3, 6][me], z := x + y)
+        return z
 
     a, b = line.strip().split(" ")
     l = ord(a) - ord("A")
     r = ord(b) - ord("X")
-    total += solve_1(r, l)
+    total += solve_2(r, l)
 
 print(total)
