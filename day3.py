37780a995a6a5fa68b440523d70a3858d9820ed9
Kittenlover229 <artur.roos@mail.ru>
2022-12-03 09:52:26 +0200

    Day 3, part 2

--- day3.py
+++ day3.py
@@ -1,16 +1,35 @@
-s = 0
-for ss in open("inputs.txt").readlines():
-    l = len(ss)
-    print(ss[:l//2], ss[l//2:])
-    a = set(ss[l//2:])
-    b = set(ss[:l//2])
-    diff = list(a.intersection(b))[0]
-    print(diff)
-    diff = ord(diff)
-    if ord("a") <= diff <= ord("z"):
-        x = diff - ord("a") + 1
-    elif ord("A") <= diff <= ord("Z"):
-        x = diff - ord("A") + 26 + 1
-    s += x
+def score(d):
+    if ord("a") <= d <= ord("z"):
+        x = d - ord("a") + 1
+    elif ord("A") <= d <= ord("Z"):
+        x = d - ord("A") + 26 + 1
+    return x
 
-print(s)
+def solve_1():
+    s = 0
+
+    for ss in open("inputs.txt").readlines():
+        l = len(ss)
+        print(ss[:l//2], ss[l//2:])
+        a = set(ss[l//2:])
+        b = set(ss[:l//2])
+        diff = list(a.intersection(b))[0]
+        print(diff)
+        d = ord(diff)
+        s += score(d)
+    print(s)
+
+def solve_2():
+    k = []
+    s = 0
+    for ss in open("inputs.txt").readlines():
+        k.append(ss.strip())
+        if len(k) != 3:
+            continue
+        a, b, c = map(set, k)
+        x = a.intersection(b).intersection(c)
+        s += score(ord(list(x)[0]))
+        k.clear()
+    print(s)
+
+solve_2()
