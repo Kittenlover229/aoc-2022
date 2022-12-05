93f4c4160e577625569cbe9459534a32f850f434
Kittenlover229 <artur.roos@mail.ru>
2022-12-05 16:37:59 +0200

    Day 4, part 2

--- day4.py
+++ day4.py
@@ -14,4 +14,22 @@ def solve_1():
             c += 1
     print(c)
 
-solve_1()
+def solve_2():
+    c = 0
+    for line in open("inputs.txt").readlines():
+        ls = list(
+            map(
+                lambda y: tuple(map(int, y.split("-"))),
+                line.strip().split(","),
+            )
+        )
+        a, b = ls
+        if a > b:
+            a, b = b, a
+
+        if (a[0] <= b[0] and a[1] <= b[1] and a[1] >= b[0]) or (b[0] <= a[0] and b[1] <= a[1] and b[1] >= a[0]) or (a[0] >= b[0] and a[1] <= b[1]) or (a[0] <= b[0] and a[1] >= b[1]):
+            print(a, b)
+            c += 1
+    print(c)
+
+solve_2()
