b22d0a754c6ddf435b5a864147a3b89d975678c3
Kittenlover229 <artur.roos@mail.ru>
2022-12-06 12:48:26 +0200

    Day 6, part 2

--- day6.py
+++ day6.py
@@ -1,7 +1,11 @@
-x = open('inputs.txt').read()
+def make_solve(z: int):
+    x = open('inputs.txt').read()
+    def solve():
+        for i in range(1, len(x) - z):
+            y = len(set(x[i:i+z]))
+            if y == z:
+                print(i + z)
+                break
+    return solve
 
-for i in range(1, len(x) - 4):
-    y = len(set(x[i:i+4]))
-    if y == 4:
-        print(i + 4)
-        break
+make_solve(14)()
